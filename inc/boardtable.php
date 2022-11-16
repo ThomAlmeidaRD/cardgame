4a019860322c538b569841d3bdc2ed03fb279618
thomas almeida <thomas.rodrigues@idealtrends.com.br>
2022-11-15 22:15:50 -0300

    demo board

---
+++ inc/boardtable.php
@@ -0,0 +1,49 @@
+<div class="boardtable">
+    <div id="tridiv">
+        <div class="scene" style="-webkit-transform:rotateX(-42deg) rotateY(0deg); -moz-transform:rotateX(-42deg) rotateY(0deg); -ms-transform:rotateX(-42deg) rotateY(0deg); transform:rotateX(-42deg) rotateY(0deg); ">
+            <div class="shape cuboid-1 cub-1">
+                <div class="face ft">
+                    <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.06);"></div>
+                </div>
+                <div class="face bk">
+                    <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.06);"></div>
+                </div>
+                <div class="face rt">
+                    <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.06);"></div>
+                </div>
+                <div class="face lt">
+                    <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.06);"></div>
+                </div>
+                <div class="face bm">
+                    <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.06);"></div>
+                </div>
+                <div class="face tp">
+                    <div class="photon-shader" id="background" style="background-color: rgba(0, 0, 0, 0.06);">
+                        <div class="card-slots">
+                            <div>
+                                <ul>
+                                    <li class="card-slot"></li>
+                                    <li class="card-slot"></li>
+                                    <li class="card-slot"></li>
+                                    <li class="card-slot"></li>
+                                </ul>
+                            </div>
+                            <div>
+                                <ul>
+                                    <li class="card-slot"></li>
+                                    <li class="card-slot"></li>
+                                    <li class="card-slot"></li>
+                                    <li class="card-slot"></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                   
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<? include('inc/card-hands.php') ?>
---
+++ inc/card-hands.php
@@ -0,0 +1,36 @@
+<div class="card-hand">
+    <ul>
+        <li class="hand-card" id="card-1">
+            <div class="card-cover" onmouseover="descript(this)">
+                <div>
+                    <h3>Gatos Gêmeos</h3>
+                    <p>Cause um efeito negativo no oponente.</p>
+                </div>
+            </div>
+        </li>
+        <li class="hand-card" id="card-2">
+            <div class="card-cover" onmouseover="descript(this)">
+                <div>
+                    <h3>Ero Sennin</h3>
+                    <h3>5/3<h3>
+                </div>
+            </div>
+        </li>
+        <li class="hand-card" id="card-3">
+            <div class="card-cover" onmouseover="descript(this)">
+                <div>
+                    <h3>Sandaime<h3>
+                            <h3>9/3<h3>
+                </div>
+            </div>
+        </li>
+        <li class="hand-card" id="card-4">
+            <div class="card-cover" onmouseover="descript(this)">
+                <div>
+                    <h3>Oni</h3>
+                    <h3>5/3<h3>
+                </div>
+            </div>
+        </li>
+    </ul>
+</div>
